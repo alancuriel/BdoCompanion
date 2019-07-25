b5c582d0029117004f0e7c9eb45070e9b9ba12dc
Alan Curiel <alannitro17@gmail.com>
2019-07-25 12:31:41 -0400

    Add In Game Time
    Added a dispatch timer that calculate the in game
    time every in game minute. Changed some resources.

--- uwpUI/ViewModels/MainViewModel.cs
+++ uwpUI/ViewModels/MainViewModel.cs
@@ -9,19 +9,27 @@ namespace uwpUI.ViewModels
 {
     public class MainViewModel : Screen
     {
-        public int Incrementer { get; set; } = 0;
         public MainViewModel()
         {
         }
 
-        private string _time = "0";
+        private bool _isDay;
+
+        public bool IsDay
+        {
+            get { return _isDay; }
+            set { _isDay = value; }
+        }
+
+
+        private string _time = string.Empty;
 
         public string Time
         {
             get { return _time; }
             set
             {
-                Set(ref _time, value);
+                Set(ref _time, $"Game Time: {value}");
             }
         }
 
@@ -29,17 +37,61 @@ namespace uwpUI.ViewModels
         protected override void OnInitialize()
         {
             base.OnInitialize();
-            DispatcherTimer timer = new DispatcherTimer();
-            timer.Interval = TimeSpan.FromSeconds(1);
+            DispatcherTimer clockTimer = new DispatcherTimer();
+            clockTimer.Interval = TimeSpan.FromMilliseconds(100);
+            clockTimer.Tick += InitTick;
+            clockTimer.Start();
+        }
+
+        private void InitTick(object sender, object e)
+        {
+            OnTick(sender, e);
+
+            var timer = (DispatcherTimer)sender;
+            timer.Stop();
+            timer.Interval = TimeSpan.FromMilliseconds(4444.444444);
             timer.Tick += OnTick;
             timer.Start();
         }
 
         private void OnTick(object sender, object e)
         {
-            Incrementer++;
-            Time = Incrementer.ToString();
-            //NotifyOfPropertyChange(Time);
+            //Code used from SomethingLovely.net
+            var utcNow = DateTime.UtcNow;
+            var startHr = utcNow - new TimeSpan((int)utcNow.DayOfWeek, utcNow.Hour, utcNow.Minute, utcNow.Second, utcNow.Millisecond);
+            var rlDayElapsed = (utcNow - startHr).TotalSeconds;
+            var secsIntoGameDay = (rlDayElapsed + 200 * 60 + 20 * 60) % (240 * 60);
+
+
+            int inGameHour;
+            int inGameMinute;
+
+            if(secsIntoGameDay >= 12000)
+            {
+                var secsIntoGameNight = secsIntoGameDay - 12000;
+                var pctOfNightDone = secsIntoGameNight / (40 * 60);
+                var gameHour = 9 * pctOfNightDone;
+                gameHour = gameHour < 2 ? 22 + gameHour : gameHour - 2;
+                var secsUntilNightEnd = 40 * 60 - secsIntoGameNight;
+
+                IsDay = false;
+                inGameHour = (int)(gameHour / (1 >> 0));
+                inGameMinute = (int)(gameHour % 1 * (60 >> 0));
+            }
+            else
+            {
+                var secsIntoGameDaytime = secsIntoGameDay;
+                var pctOfDayDone = secsIntoGameDay / (200 * 60);
+                var gameHour = 7 + (22 - 7) * pctOfDayDone;
+                var secsUntilNightStart = 12000 - secsIntoGameDaytime;
+
+                IsDay = true;
+                inGameHour = (int)(gameHour / (1 >> 0));
+                inGameMinute = (int)(gameHour % 1 * (60 >> 0));
+            }
+
+            var inGameTime = new TimeSpan(inGameHour, inGameMinute,0);
+            Time = DateTime.Today.Add(inGameTime).ToString("h:m tt");
         }
     }
 }
