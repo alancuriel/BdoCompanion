260e6a7d3db70e95e04048ff7adc2f7d49af5162
Alan Curiel <alannitro17@gmail.com>
2019-07-25 18:17:48 -0400

    Add reset Countdown timers
    Added calculations for the reset timers in game and added more to resources.

--- uwpUI/ViewModels/MainViewModel.cs
+++ uwpUI/ViewModels/MainViewModel.cs
@@ -33,30 +33,120 @@ namespace uwpUI.ViewModels
             }
         }
 
+        private string _dailyReset;
+
+        public string DailyReset
+        {
+            get { return _dailyReset; }
+            set
+            {
+                Set(ref _dailyReset, $"Daily reset in: {value}");
+            }
+        }
+
+        private string _imperialReset;
+
+        public string ImperialReset
+        {
+            get { return _imperialReset; }
+            set
+            {
+                Set(ref _imperialReset, $"Imperial Reset in: {value}");
+            }
+        }
+
+        private string _imperialTradeReset;
+
+        public string ImperialTradeReset
+        {
+            get { return _imperialTradeReset; }
+            set
+            {
+                Set(ref _imperialTradeReset, $"Imperial Trade Reset in{value}");
+            }
+        }
+
+        private string _bSGameReset;
+
+        public string BSGameReset
+        {
+            get { return _bSGameReset; }
+            set
+            {
+                Set(ref _bSGameReset, $"Black Spirit Adventure Reset in: {value}");
+            }
+        }
+
+
+
 
         protected override void OnInitialize()
         {
             base.OnInitialize();
-            DispatcherTimer clockTimer = new DispatcherTimer();
-            clockTimer.Interval = TimeSpan.FromMilliseconds(100);
-            clockTimer.Tick += InitTick;
+
+            var startInterval = TimeSpan.FromMilliseconds(100);
+
+            var clockTimer = new DispatcherTimer();
+            clockTimer.Interval = startInterval;
+            clockTimer.Tick += InitTimeTick;
+
+            var countdownTimer = new DispatcherTimer();
+            countdownTimer.Interval += startInterval;
+            countdownTimer.Tick += InitCountdownTick;
+
             clockTimer.Start();
+            countdownTimer.Start();
         }
 
-        private void InitTick(object sender, object e)
+        private void InitCountdownTick(object sender, object e)
         {
-            OnTick(sender, e);
+            OnCountdownTick(sender,e);
+
+            var timer = (DispatcherTimer)sender;
+            timer.Stop();
+            timer.Interval = TimeSpan.FromSeconds(20);
+            timer.Tick += OnCountdownTick;
+            timer.Start();
+        }
+
+        private void OnCountdownTick(object sender, object e)
+        {
+            var utcNow = DateTime.UtcNow;
+            DateTime startHr = utcNow - new TimeSpan((int)utcNow.DayOfWeek, utcNow.Hour, utcNow.Minute, utcNow.Second, utcNow.Millisecond);
+            double rlDayElapsed = (utcNow - startHr).TotalSeconds;
+
+            var tmmrw = utcNow.AddDays(1).Date;
+            var secsUntilDailyReset = tmmrw.Subtract(utcNow);
+            DailyReset = secsUntilDailyReset.ToString("%h' hr '%m' min'");
+
+            var secsUntilImperialReset = TimeSpan.FromSeconds(3 * 60 * 60 - rlDayElapsed % (60 * 60 * 3));
+            ImperialReset = secsUntilImperialReset.ToString("%h' hr '%m' min'");
+
+            var secsUntilTradeReset = TimeSpan.FromSeconds(4 * 60 * 60 - rlDayElapsed % (60 * 60 * 4));
+            ImperialTradeReset = secsUntilTradeReset.ToString("%h' hr '%m' min'");
+
+            var secsUntilBSGameReset = TimeSpan.FromSeconds(60 * 60 * 24 - (rlDayElapsed - 5 * 60 * 60) % (60 * 60 * 24));
+            if (secsUntilBSGameReset > TimeSpan.FromSeconds(60 * 60 * 24))
+            {
+                secsUntilBSGameReset -= TimeSpan.FromSeconds(60 * 60 * 24);
+            }
+            BSGameReset = secsUntilBSGameReset.ToString("%h' hr '%m' min'");
+        }
+
+        private void InitTimeTick(object sender, object e)
+        {
+            OnTimeTick(sender, e);
 
             var timer = (DispatcherTimer)sender;
             timer.Stop();
             timer.Interval = TimeSpan.FromMilliseconds(4444.444444);
-            timer.Tick += OnTick;
+            timer.Tick += OnTimeTick;
             timer.Start();
         }
 
-        private void OnTick(object sender, object e)
+        private void OnTimeTick(object sender, object e)
         {
-            //Code used from SomethingLovely.net
+            //Converted code from SomethingLovely.net
             var utcNow = DateTime.UtcNow;
             var startHr = utcNow - new TimeSpan((int)utcNow.DayOfWeek, utcNow.Hour, utcNow.Minute, utcNow.Second, utcNow.Millisecond);
             var rlDayElapsed = (utcNow - startHr).TotalSeconds;
@@ -91,7 +181,7 @@ namespace uwpUI.ViewModels
             }
 
             var inGameTime = new TimeSpan(inGameHour, inGameMinute,0);
-            Time = DateTime.Today.Add(inGameTime).ToString("h:m tt");
+            Time = DateTime.Today.Add(inGameTime).ToString("h:mm tt");
         }
     }
 }
