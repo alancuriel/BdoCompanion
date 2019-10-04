04aa966967f4717dc6671735e1c97d250be14ab5
Alan Curiel <alannitro17@gmail.com>
2019-10-04 12:27:30 -0400

    Add Bdo news models
    Added news models to be aggregate news data from multiple sources into objects.

---
+++ uwpUI.Core/Models/BdoNews.cs
@@ -0,0 +1,72 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+
+namespace uwpUI.Core.Models
+{
+    public partial class NewsResult
+    {
+        [JsonProperty("result")]
+        public Result Result { get; set; }
+    }
+
+    public partial class Result
+    {
+        [JsonProperty("hashtag")]
+        public string Hashtag { get; set; }
+
+        [JsonProperty("category")]
+        public long Category { get; set; }
+
+        [JsonProperty("list")]
+        public List<NewsItem> List { get; set; }
+
+        [JsonProperty("searchType")]
+        public string SearchType { get; set; }
+
+        [JsonProperty("searchText")]
+        public string SearchText { get; set; }
+
+        [JsonProperty("pageNo")]
+        public long PageNo { get; set; }
+
+        [JsonProperty("pageSize")]
+        public long PageSize { get; set; }
+
+        [JsonProperty("totalCount")]
+        public long TotalCount { get; set; }
+    }
+
+    public partial class NewsItem
+    {
+        [JsonProperty("boardNo")]
+        public int BoardNo { get; set; }
+
+        [JsonProperty("category")]
+        public int Category { get; set; }
+
+        [JsonProperty("categoryName")]
+        public string CategoryName { get; set; }
+
+        [JsonProperty("title")]
+        public string Title { get; set; }
+
+        [JsonProperty("subTitle")]
+        public string SubTitle { get; set; }
+
+        [JsonProperty("imageURL")]
+        public Uri ImageUrl { get; set; }
+
+        [JsonProperty("thumbnailURL")]
+        public Uri ThumbnailUrl { get; set; }
+
+        [JsonProperty("regDate")]
+        public string RegDate { get; set; }
+
+        [JsonProperty("hashTags")]
+        public string HashTags { get; set; }
+
+        public string DetailUrl { get; set; }
+    }
+}
+
