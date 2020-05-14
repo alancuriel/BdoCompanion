74421f09c55b5ba8e5b97f345c515d406db06ca4
Alan Curiel <alannitro17@gmail.com>
2020-05-13 21:38:07 -0400

    Refactor Control Children Finder

---
+++ uwpUI/Helpers/FindControlHelper.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Media;
+
+namespace uwpUI.Helpers
+{
+    public static class FindControlHelper
+    {
+        public static T FindChildControl<T>(UIElement parent, Type targetType, string ControlName) where T : FrameworkElement
+        {
+
+            if (parent == null) return null;
+            if (parent.GetType() == targetType && ((T)parent).Name == ControlName) return (T)parent;
+
+            int count = VisualTreeHelper.GetChildrenCount(parent);
+            for (int i = 0; i < count; i++)
+            {
+                UIElement child = (UIElement)VisualTreeHelper.GetChild(parent, i);
+                T result = FindChildControl<T>(child, targetType, ControlName);
+                if (result != null) return result;
+            }
+            return null;
+        }
+    }
+}
--- uwpUI/Views/SecondPage.xaml.cs
+++ uwpUI/Views/SecondPage.xaml.cs
@@ -3,6 +3,7 @@ using uwpUI.ViewModels;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Media;
+using uwpUI.Helpers;
 
 namespace uwpUI.Views
 {
@@ -18,27 +19,11 @@ namespace uwpUI.Views
             get { return DataContext as SecondViewModel; }
         }
 
-        public static T FindControl<T>(UIElement parent, Type targetType, string ControlName) where T : FrameworkElement
-        {
-
-            if (parent == null) return null;
-            if (parent.GetType() == targetType && ((T)parent).Name == ControlName) return (T)parent;
-
-            int count = VisualTreeHelper.GetChildrenCount(parent);
-            for (int i = 0; i < count; i++)
-            {
-                UIElement child = (UIElement)VisualTreeHelper.GetChild(parent, i);
-                T result = FindControl<T>(child, targetType, ControlName);
-                if (result != null) return result;
-            }
-            return null;
-        }
-
         private void Bosses_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
             GridView item = (sender as GridView);
             GridViewItem gvitem = item.ContainerFromItem(item.SelectedItem) as GridViewItem;
-            CheckBox CheckBox = FindControl<CheckBox>(gvitem, typeof(CheckBox), "BossSwitch");
+            CheckBox CheckBox = FindControlHelper.FindChildControl<CheckBox>(gvitem, typeof(CheckBox), "BossSwitch");
             CheckBox?.Focus(FocusState.Programmatic);
         }
 
