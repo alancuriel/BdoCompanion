dc7d428e9b6fba12b851bed4ecff1bd37714b36e
Alan Curiel <alannitro17@gmail.com>
2020-05-15 18:31:34 -0400

    Fix PC SEA News Bug

--- uwpUI.Core/Services/BdoNewsDataService.cs
+++ uwpUI.Core/Services/BdoNewsDataService.cs
@@ -20,6 +20,9 @@ namespace uwpUI.Core.Services
         {
             await Task.CompletedTask;
             Client = new HttpClient();
+            Client.DefaultRequestHeaders.TryAddWithoutValidation("accept-language", "en-US,en;q=0.9");
+            Client.DefaultRequestHeaders
+                .TryAddWithoutValidation("user-agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.138 Safari/537.36");
             HtmlDocument = new HtmlDocument();
         }
 
